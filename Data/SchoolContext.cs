abc2f4aa17e9fcbd6f957a6b92c9bb3329c23674
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2022-06-13 15:52:05 -0400

    Overrided OnModelCreating to change table names to singular versions of the plural DbSets property names.

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Data
 {
@@ -6,6 +7,17 @@ namespace ContosoUniversity.Data
     {
         public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
         {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
         }
     }
 }
