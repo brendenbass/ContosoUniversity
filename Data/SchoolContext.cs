cfed8279e076e7acadfbc780c82aef2897fb9d20
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2022-06-13 15:41:08 -0400

    Add project files.

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+    }
+}
